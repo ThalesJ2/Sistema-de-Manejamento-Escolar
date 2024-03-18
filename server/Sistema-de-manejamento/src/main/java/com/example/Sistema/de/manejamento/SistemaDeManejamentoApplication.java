3f7c8c22d8d223e3770383712604378859b31592
ThalesJ2 <thalesjorge36@gmail.com>
2024-03-18 08:58:21 -0300

    first commit

---
+++ server/Sistema-de-manejamento/src/main/java/com/example/Sistema/de/manejamento/SistemaDeManejamentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sistema.de.manejamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeManejamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeManejamentoApplication.class, args);
+	}
+
+}
---
+++ server/Sistema-de-manejamento/src/test/java/com/example/Sistema/de/manejamento/SistemaDeManejamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sistema.de.manejamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeManejamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
