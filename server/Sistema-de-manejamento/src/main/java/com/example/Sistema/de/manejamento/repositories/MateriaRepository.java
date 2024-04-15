503f6dba61702b68df4a51ce5ab47413788be4da
Aluno <thalesjorge36@gmail.com>
2024-04-15 09:22:40 -0300

    listagem de materias

---
+++ server/Sistema-de-manejamento/src/main/java/com/example/Sistema/de/manejamento/repositories/MateriaRepository.java
@@ -0,0 +1,10 @@
+package com.example.Sistema.de.manejamento.repositories;
+
+import com.example.Sistema.de.manejamento.entities.Materia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface MateriaRepository extends JpaRepository<Materia,Integer> {
+}
