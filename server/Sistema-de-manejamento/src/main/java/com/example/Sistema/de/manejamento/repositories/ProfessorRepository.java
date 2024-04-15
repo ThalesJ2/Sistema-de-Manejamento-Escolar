5d15b72d1f7bae8c1bf251ffb1e0e571ed015c3c
Aluno <thalesjorge36@gmail.com>
2024-04-15 09:03:56 -0300

    comentando codigo do professor.

--- server/Sistema-de-manejamento/src/main/java/com/example/Sistema/de/manejamento/repositories/ProfessorRepository.java
+++ server/Sistema-de-manejamento/src/main/java/com/example/Sistema/de/manejamento/repositories/ProfessorRepository.java
@@ -1,11 +1,11 @@
-package com.example.Sistema.de.manejamento.repositories;
-
-import com.example.Sistema.de.manejamento.entities.Atividade;
-import com.example.Sistema.de.manejamento.entities.Professor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProfessorRepository extends JpaRepository<Professor,Integer>{
-
-}
+//package com.example.Sistema.de.manejamento.repositories;
+//
+//import com.example.Sistema.de.manejamento.entities.Atividade;
+//import com.example.Sistema.de.manejamento.entities.Professor;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface ProfessorRepository extends JpaRepository<Professor,Integer>{
+//
+//}
