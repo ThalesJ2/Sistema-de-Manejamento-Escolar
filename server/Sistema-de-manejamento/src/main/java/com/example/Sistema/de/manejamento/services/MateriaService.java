4a830c4e862364ac9469e394fe66630e15b837bc
daniel <thalesjorge36@gmail.com>
2024-04-18 09:00:21 -0300

    adicionando crud de materias

--- server/Sistema-de-manejamento/src/main/java/com/example/Sistema/de/manejamento/services/MateriaService.java
+++ server/Sistema-de-manejamento/src/main/java/com/example/Sistema/de/manejamento/services/MateriaService.java
@@ -1,12 +1,15 @@
 package com.example.Sistema.de.manejamento.services;
 
 
-import com.example.Sistema.de.manejamento.entities.Aluno;
 import com.example.Sistema.de.manejamento.entities.Materia;
 import com.example.Sistema.de.manejamento.repositories.MateriaRepository;
+import com.example.Sistema.de.manejamento.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.List;
 
@@ -21,5 +24,33 @@ public class MateriaService {
     @Transactional(readOnly = true)
     public List<Materia> findAll(){
         return  materiaRepository.findAll();
+    }
+
+    @Transactional
+    public Materia create(Materia materia){
+        return  materiaRepository.save(materia);
+    }
+    @Transactional
+    public void delete(Integer id){
+        if(!materiaRepository.existsById(id))
+            throw new ResourceNotFoundException("Materia não encontrado");
+        materiaRepository.deleteById(id);
+
+    }
+    @Transactional
+    public Materia update(Integer id, Materia materia){
+        try{
+            Materia nova = materiaRepository.getReferenceById(id);
+
+            nova.setNome(materia.getNome());
+
+
+            nova =  materiaRepository.save(nova);
+
+            return nova;
+        }
+        catch(EntityNotFoundException e){
+            throw new ResourceNotFoundException("Materia não encontrada.");
+        }
     }
 }
